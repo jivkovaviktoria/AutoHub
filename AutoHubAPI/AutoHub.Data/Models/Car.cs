3350456999c08b3e70894cc1c91ea63539dabbc9
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-04 13:29:04 +0200

    Created the database

---
+++ AutoHubAPI/AutoHub.Data/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoHub.Data.Models;
+
+public class Car : BaseEntity
+{
+    public string Model { get; set; } = null!;
+    public string Brand { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Year { get; set; }
+    public string ImageUrl { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public bool IsSold { get; set; } = false;
+}
