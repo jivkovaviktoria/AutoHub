e3a2032a305c7135dfdf1da8e8b6ec007b5e7b08
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-14 09:53:26 +0200

    Added style for the car info window

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -4,7 +4,19 @@ export const Info = ({car, onClose}) => {
     return (
         <div className={styles.overlay} onClick={onClose}>
             <div className={styles['info-window']}>
-                <h1>{car.id}</h1>
+                <div className={styles.content}>
+                    <div className={styles['car-details']}>
+                        <p>
+                            <strong className={styles.heading}> {car.model} {car.brand} </strong>
+                        </p>
+                        <p>Year: <strong>{car.year}</strong></p>
+                        <p>Price: <strong>{car.price}</strong></p>
+                        <p>
+                            <strong>{car.description}</strong>
+                        </p>
+                        <img src={car.imageUrl}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
