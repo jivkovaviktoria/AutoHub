7e38b5e8ae197ad29d18a38c08d02b0a8254809f
ViktoriyaV <jivkovaviktoria@gmail.com>
2023-06-11 17:28:14 +0300

    Added tests for the OperationResult class

---
+++ AutoHubAPI/AutoHub.Tests/OperationResultTests/OperationResultTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoHub.Utilities;
+
+namespace AutoHub.Tests.OperationResultTests;
+
+public class OperationResultTests
+{
+    [Fact]
+    public void OperationResultShouldBeInitiallySuccessful()
+    {
+        var operationResult = new OperationResult();
+
+        Assert.True(operationResult.IsSuccessful);
+        Assert.Equal(0, operationResult.Errors.Count);
+    }
+
+    [Fact]
+    public void OperationResultWithErrorsShouldBeUnsuccessful()
+    {
+        var operationResult = new OperationResult();
+
+        var result = operationResult.AddError(new Error());
+
+        Assert.True(result);
+        Assert.False(operationResult.IsSuccessful);
+        Assert.Equal(1, operationResult.Errors.Count);
+    }
+
+    [Fact]
+    public void AddingNullErrorShouldBeFalse()
+    {
+        var operationResult = new OperationResult();
+
+        var result = operationResult.AddError(null);
+        
+        Assert.False(result);
+        Assert.True(operationResult.IsSuccessful);
+        Assert.Equal(0, operationResult.Errors.Count);
+    }
+
+    [Theory]
+    [InlineData(10)]
+    [InlineData(20)]
+    [InlineData(30)]
+    public void OperationResultShouldStoreDataCorectly(int data)
+    {
+        var operationResult = new OperationResult<int>(){Data = data};
+        Assert.Equal(operationResult.Data, data);
+    }
+}
